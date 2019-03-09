008aea5e909e5255403c9d1d6e172b70b1e63584
Marw <marwan.azizi@gmail.com>
2019-03-09 11:01:00 +0100

    set up webpack-dev-server

--- src/components/test.js
+++ src/components/test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Mod = () => {
-    return <h1>Hello World !</h1>
+    return <h1>Hello World ! How are you ?</h1>
 };
 
 export default Mod;
--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,33 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: { app: './src/entry.js' },
+
     output: {
         path: __dirname,
         filename: './src/dist/entry.dist.js'
     },
+
     module: {
-        rules: [
-        {
+        rules: [{
             test: /\.js$/,
             exclude: /node_modules/,
             use: {
                 loader: 'babel-loader'
             }
-        },
-        {
+        }, {
             test: /\.css$/,
             use: ['css-loader', 'style-loader']
-        }
-        ]
+        }]
     },
+
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: 'public/index.dist.html'
         })
-    ]
+    ],
+
+    devServer: {
+        openPage: '/public/index.dist.html'
+    }
 };
